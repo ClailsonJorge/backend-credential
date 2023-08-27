f2f1a08917e57281d330a4a2a234a12696a61456
clailsonjorge <clailson@notebook_dell.localdomain>
2023-08-27 14:37:52 -0300

    Ensure cors

--- index.ts
+++ index.ts
@@ -3,11 +3,25 @@ import * as express from 'express';
 import { userRouter } from "./src/routes/user";
 import { loginRouter } from "./src/routes/login";
 import { credentialsRouter } from "./src/routes/credentials";
+import * as cors from 'cors';
 
 const app = express();
 const PORT = 3001;
 
+const consoleMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log({
+        url: req.url,
+        body: req.body,
+        params: req.params,
+        queries: req.query
+    })
+
+    next();
+}
+
+app.use(cors());
 app.use(express.json());
+app.use(consoleMiddleware)
 
 app.use("/user", userRouter);
 app.use("/login", loginRouter);
